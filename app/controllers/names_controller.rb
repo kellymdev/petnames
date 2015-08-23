442a52e2db5e623494a308fb2d643cdbb988e13b
Kelly Munro <kellymdev@gmail.com>
2015-08-24 10:05:15 +1200

    Return a list of names for a letter as json

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -4,4 +4,9 @@ class NamesController < ApplicationController
     list = ('A'..'Z').to_a
     render json: list
   end
+
+  def by_letter
+    names = Name.where("name LIKE ?", "#{params[:letter]}%")
+    render json: names
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/names/alphabetical/index', to: 'names#alphabetical', as: 'alphabetical_index'
+  get '/names/alphabetical/:letter', to: 'names#by_letter', as: 'alphabetical_by_letter'
 end

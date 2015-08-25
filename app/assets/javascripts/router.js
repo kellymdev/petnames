eb4faa2fb548aaa534df6cdc99b9ef6388f7f0c1
Kelly Munro <kellymdev@gmail.com>
2015-08-25 15:31:19 +1200

    Add hyperlinks to names on the random names list

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
     nameController.nameDetailsRequest(url);
   });
 
+  $('#content').on('click', '.random-names a', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    nameController.nameDetailsRequest(url);
+  });
+
   $('#content').on('click', '.random-button', function(e) {
     e.preventDefault();
     var url = $(this).attr('href');
